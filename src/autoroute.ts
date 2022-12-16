3f1cb058d8aa4145ea71716dfbe7cf91b0e62e65
jasonhoi <jason@de3interactive.com>
2022-12-16 23:17:13 +0800

    W.I.P. need to fix JSBI.BigInt conversion, add npm run quote (auto route)

--- src/autoroute.ts
+++ src/autoroute.ts
@@ -1,80 +1,60 @@
 /**
- * Tutorial: https://docs.uniswap.org/sdk/v3/guides/creating-a-trade
- *
- * import { abi as IUniswapV3PoolABI } from '@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json' assert { type: 'json' };
- * import { abi as QuoterABI } from '@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json' assert { type: 'json' };
- * @dev In Typescript Node project, importing json files as ES module is highly experimental
- *      at this moment, I manually copy out the ABI from /node_modules/@uniswap/v3-* into /scr/abi/*.json.
+ * Tutorial: https://docs.uniswap.org/sdk/v3/guides/auto-router
+ * Uniswap smart order router src: https://github.com/Uniswap/smart-order-router
  *
  */
 
 // Always try to put your api keys or secrets into .env file, and git ignore the file from soure code repo.
 import { ethers } from "ethers";
-import { Token } from "@uniswap/sdk-core";
-import IUniswapV3PoolABI from "./abi/IUniswapV3Pool.json" assert { type: "json" };
-import QuoterABI from "./abi/Quoter.json" assert { type: "json" };
-import ERC20ABI from "./abi/ERC20.json" assert { type: "json" };
-import * as Uniswap from "./services/uniswap.js";
-import * as ERC20 from "./services/erc20.js";
+import { AlphaRouter, SwapType } from "@uniswap/smart-order-router";
+import { Token, CurrencyAmount, TradeType, Percent } from "@uniswap/sdk-core";
+import Tokens from "./tokens/uniswap-default/mainnet.json" assert { type: "json" };
 import * as dotenv from "dotenv";
+import JSBI from "jsbi";
 dotenv.config();
 
 // shortcut alias
 const parseUnits = ethers.utils.parseUnits;
 
-// ETH RPC network provider
-// const provider = new ethers.providers.JsonRpcProvider(process.env.MAINNET_RPC);
-const provider = new ethers.providers.JsonRpcProvider("https://rpc.flashbots.net");
-
-// connect to Uniswap v3 Quoter contract
-const quoterAddress = "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6";
-const quoterContract = new ethers.Contract(quoterAddress, QuoterABI, provider);
-
-// connect to Uniswap v3 pool contract
-// @SEE https://info.uniswap.org/#/pools to search for pool contract address
-// 0xc63b0708e2f7e69cb8a1df0e1389a98c35a76d52 = FRAX/USDC
-// 0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8 = USDC/ETH
-// 0xcbcdf9626bc03e24f779434178a73a0b4bad62ed = WBTC/ETH
-const poolAddress = "0xa6cc3c2531fdaa6ae1a3ca84c2855806728693e8";
-const poolContract = new ethers.Contract(poolAddress, IUniswapV3PoolABI, provider);
+// params
+const provider = new ethers.providers.JsonRpcProvider(process.env.MAINNET_RPC);
+const MY_ADDRESS = process.env.MY_WALLET;
+const V3_SWAP_ROUTER_ADDRESS = "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45";
 
 async function main() {
-  // query the state and immutable variables of the pool
-  const [immutables, state] = await Promise.all([
-    Uniswap.getPoolImmutables(poolContract),
-    Uniswap.getPoolState(poolContract),
-  ]);
+  const router = new AlphaRouter({ chainId: 1, provider: provider });
+  const TokenA = Tokens.find((x) => x.symbol == "WETH");
+  const TokenB = Tokens.find((x) => x.symbol == "USDC");
 
-  const token0Contract = new ethers.Contract(immutables.token0, ERC20ABI, provider);
-  const token1Contract = new ethers.Contract(immutables.token1, ERC20ABI, provider);
-  const [token0, token1] = await Promise.all([
-    ERC20.getToken(token0Contract),
-    ERC20.getToken(token1Contract),
-  ]);
-
-  const TokenIn = new Token(1, immutables.token0, token0.decimals, token0.symbol, "");
-  const TokenOut = new Token(1, immutables.token1, token1.decimals, token1.symbol, "");
+  const TokenIn = new Token(
+    TokenA.chainId,
+    TokenA.address,
+    TokenA.decimals,
+    TokenA.symbol,
+    TokenA.name
+  );
+  const TokenOut = new Token(
+    TokenB.chainId,
+    TokenB.address,
+    TokenB.decimals,
+    TokenB.symbol,
+    TokenB.name
+  );
 
   // assign an input amount for the swap
   const amountInFloat = 10;
   const amountIn = parseUnits(amountInFloat.toFixed(4), TokenIn.decimals);
+  const wethAmount = CurrencyAmount.fromRawAmount(TokenIn, JSBI.BigInt(amountIn));
 
-  // call the quoter contract to determine the amount out of a swap, given an amount in
-  // Uniswap V3 provide at least 4 fee tiers: 100 = 0.01%(very stable pair), 500 = 0.05%, 3000 = 0.3%, 10000 = 1%
-  const quotedAmountOut = await quoterContract.callStatic.quoteExactInputSingle(
-    TokenIn.address,
-    TokenOut.address,
-    immutables.fee,
-    amountIn.toString(),
-    0
-  );
+  const route = await router.route(wethAmount, TokenIn, TradeType.EXACT_INPUT, {
+    recipient: MY_ADDRESS,
+    slippageTolerance: new Percent(5, 100),
+    type: SwapType.UNIVERSAL_ROUTER,
+  });
 
-  const quoteDecimalPlace = 4;
-  const quotedAmountOutFloat =
-    parseFloat(
-      quotedAmountOut.div(parseUnits("1", TokenOut.decimals - quoteDecimalPlace)).toString()
-    ) /
-    10 ** quoteDecimalPlace;
+  console.log(`Quote Exact In: ${route.quote.toFixed(2)}`);
+  console.log(`Gas Adjusted Quote In: ${route.quoteGasAdjusted.toFixed(2)}`);
+  console.log(`Gas Used USD: ${route.estimatedGasUsedUSD.toFixed(6)}`);
 
   console.log("--------------------------- Uniswap V3 exchange quote ---------------------------");
   console.log(
@@ -84,13 +64,10 @@ async function main() {
     `Token Out: ${TokenOut.symbol}, decimal place ${TokenOut.decimals}, contract ${TokenOut.address}`
   );
   console.log(
-    `${TokenIn.symbol} : ${TokenOut.symbol} = ${amountInFloat.toFixed(
-      4
-    )} : ${quotedAmountOutFloat.toFixed(quoteDecimalPlace)}`
+    `${TokenIn.symbol} : ${TokenOut.symbol} = ${amountInFloat.toFixed(4)} : ${route.quote.toFixed(
+      2
+    )}`
   );
-  console.log(`Ex. rate (out amount/in amount) = ${quotedAmountOutFloat / amountInFloat}`);
-  console.log(`Pool fee tier: ${immutables.fee / 10000}%`);
-  console.log(`Pool liquidity: ${state.liquidity.toString()}`);
 }
 
 main();
